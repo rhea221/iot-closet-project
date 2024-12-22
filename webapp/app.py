b929973c0ea983cad837d0b5a94c882a2729a96a
np221 <np221@ic.ac.uk>
2024-12-22 17:49:09 +0000

    v3

--- webapp/app.py
+++ webapp/app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import requests
 
 # Loading environment variables
-load_dotenv(dotenv_path="/workspaces/iot-closet-project/config/.env")
+load_dotenv(dotenv_path="iot-closet-project/config/.env")
 
 # Supabase Data Storage ----------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------------------
